b3c813f94949680a84a8b26b87d6c5cd611e9bd2
elarity <42677833@qq.com>
2019-07-25 20:14:40 +0800

    添加hash函数

--- clang.hash-table.c
+++ clang.hash-table.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 // 用这个struct存储键值对
 // 比如 username:"xiaodushe"
@@ -33,9 +34,9 @@ hashtable_item * allocate_new_item( char * key, char * item ) {
 // return : 指向hashtable的指针
 hashtable * allocate_hashtable( int size ) {
   hashtable * pt_ht = ( hashtable * )malloc( sizeof( hashtable ) ); 
-  pt_ht->size    = size;
-  pt_ht->counter = 0;
-  pt_ht->item    = calloc( pt_ht->size, sizeof( hashtable_item * ) );
+  pt_ht->size       = size;
+  pt_ht->counter    = 0;
+  pt_ht->item       = calloc( pt_ht->size, sizeof( hashtable_item * ) );
   return pt_ht;
 }
 
@@ -65,10 +66,52 @@ void free_hashtable( hashtable * pt_ht ) {
   free( pt_ht );
 }
 
+// 网上广为流传的time33 hash算法...反正能用
+uint time33( char * str, const int size ) {
+  uint hash = 5381;
+  while( *str ){
+    hash += ( hash << 5 ) + ( *str++ );
+  }
+  hash = ( hash & 0x7FFFFFFF );
+  // 此处沙雕地改了一下，凑合看...正式用千万别这么搞，可能会被人打死
+  hash = hash % size;
+  return hash;
+}
+
+// 插入数据
+// param : hashtable_item *
+void insert_item_hashtable( hashtable_item * pt_ht_item, hashtable * pt_hashtable ) {
+  // 检测hashtable容量是否已经满了
+  if ( pt_hashtable->counter == pt_hashtable->size ) {
+    printf( "hashtable容量已经满了.\n" );
+    return;
+  }
+  // 计算这个item键值对在hashtable中的位置。
+  uint index = time33( pt_ht_item->key, pt_hashtable->size );
+  //printf( "%u\n", index );
+  // 检测index位置上是否已经有元素
+  // 如果有元素，则查找下一个元素位置
+  // item键值对的指针就存在这里:pt_hashtable->item
+  // 偏移量从0开始 
+   
+}
+
+// 删除数据
+void delete_item_hashtable() {
+}
+
 int main( int argc, char * argv[] ) {
   // 初始化一个hashtable，容量为可以存储20个item键值对
   int size = 20;
   hashtable * pt_hashtable = allocate_hashtable( size ); 
+  // 初始化一个item键值对.
+  char * key  = "username";
+  char * item = "xiaodushe";
+  hashtable_item * pt_ht_item = allocate_new_item( key, item );
+  // 将item键值对插入到hashtable中去
+  insert_item_hashtable( pt_ht_item, pt_hashtable );
+  // 清空这个hashtable-item
+  free_hashtable_item( new_ht_item );
   // 清空这个hashtable
   free_hashtable( pt_hashtable ); 
 }
